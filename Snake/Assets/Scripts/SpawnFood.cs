217cee478dd70d4fb46a26a531c2477875b18498
Anna <Anutka_Bologoe@mail.ru>
2021-02-25 12:09:26 +0300

    add snake and food

---
+++ Snake/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    [Header("Food")]
+    public GameObject foodPrefab;
+
+    [Header("Borders")]
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+
+}
